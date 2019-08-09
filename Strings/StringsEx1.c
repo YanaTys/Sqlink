a47149f764a81e9fe07ab487789c472eedbd134d
YanaTys <53776902+YanaTys@users.noreply.github.com>
2019-08-09 12:58:25 +0300

    Add files via upload

---
+++ Strings/StringsEx1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+void ItoB( unsigned  int );
+int BtoI(char*);
+
+int main()
+
+{ unsigned  int num=0;
+char binarr[10]; 
+printf("Enter int \n");
+scanf("%u",&num);
+ItoB(num);
+printf("Print string 0 or 1\n");
+scanf("%s",binarr);
+printf("num=%d\n",BtoI(binarr));
+}
+
+
+void ItoB( unsigned  int num)
+{
+int i=num/2;
+
+char arr[100];
+
+for(i=0;num>0;i++)
+{
+	arr[i]=num%2+'0';
+	num=num/2;
+}
+arr[i]='\0';
+for(i=strlen(arr)-1;i>=0;i--)
+{
+	printf("%c",arr[i]);
+}
+printf("\n");
+return;
+}
+
+
+int BtoI( char* binarr)
+{
+int i=0,num=0,j=strlen(binarr)-1;
+
+for(i=strlen(binarr)-1;i>=0;i--)
+{
+num=num+(pow(2,(j-i))*(binarr[i]-'0'));
+}
+return num;
+}
+
