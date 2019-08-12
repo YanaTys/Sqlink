e55f81ef58b011cec7b8870635943770b79dbae0
YanaTys <53776902+YanaTys@users.noreply.github.com>
2019-08-12 10:45:15 +0300

    Add files via upload

---
+++ Malloc/da2.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "da2.h"
+DA_T* createDA(int cap)
+{
+	DA_T* da;
+	da=malloc(sizeof(DA_T));
+	if(da == NULL)
+	{
+		return NULL;
+	}
+	da->cap=cap;
+	da->index=0;
+	da->ip=malloc(sizeof(int*)*cap);
+	if(da->ip == NULL)
+	{
+		free(da);
+		return NULL;
+	}
+	return da;
+}
+
+
+int insert(DA_T* da,int* num)
+{
+	int** temp;
+	if(da != NULL)
+		{
+		temp=da->ip;
+		if((da->index)==(da->cap))
+		{
+			temp=realloc(da->ip,((da->cap)*2)*sizeof(int*));
+			if(temp!=NULL)
+
+			{	da->ip=temp;
+				(da->cap)*=2;
+			}
+		
+			else 
+			{
+				return 0;
+			}
+		}
+		if (num != NULL)
+		{
+			da->ip[(da->index)]=num;
+			(da->index)++;
+			return 1;
+		}
+	}
+return 0;
+}
+void printDA(DA_T* da)
+{
+int i=0;
+	if(da != NULL)
+	{
+		for(i=0;i<da->index;i++)
+		{
+		printf("%d ",*(da->ip[i]));	
+		}
+	}
+}
+void destroyDA(DA_T* da)
+{ int i=0;
+	if(da!= NULL)
+	{
+		for (i=0;i<da->index;i++)
+		{
+			if(da->ip[i] !=NULL)
+			{
+				free(da->ip[i]);
+			}
+		}
+		free(da->ip);
+		free(da);
+	}
+}
+
---
+++ Malloc/da2.h
@@ -0,0 +1,15 @@
+#ifndef DA_T1
+#define DA_T1
+
+typedef struct {
+int index;
+int cap;
+int** ip;
+} DA_T;
+
+DA_T* createDA(int cap);
+int insert(DA_T* daptr,int*num);
+void printDA(DA_T*);
+void destroyDA(DA_T*);
+
+#endif
