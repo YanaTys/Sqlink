de94e5028697444532d3655f635993fc2fda5272
YanaTys <53776902+YanaTys@users.noreply.github.com>
2019-08-20 21:41:09 +0300

    Update darray_intTest.c

--- DArray/darray_intTest.c
+++ DArray/darray_intTest.c
@@ -15,24 +15,25 @@ int main()
         scanf("%d",&item); 
         darrayAdd(ptrDarr,item);
     } while(item !=-1);
-   
-   /*for(i =0 ; i<initial_capacity;i++)
-        {printf("Enter item to delete\n");
-        scanf("%d",&item); 
-        darrayDelete(ptrDarr,&item);
-      }*/
+    printDarray(ptrDarr);
+  
+         
+    darrayDelete(ptrDarr,&item);
+    printDarray(ptrDarr);
+    darrayDelete(ptrDarr,&item);
+    printDarray(ptrDarr);
+      
     printf("Enter index to get\n");
     scanf("%d",&index); 
     darrayGet(ptrDarr, index, &item);
-    printf("item = %d\n",item);
+    printf("item=&d\n",item);
     printf("Enter index to set\n");
     scanf("%d",&index); 
-    /*drraySet(ptrDarr, index,item);
-    darrayGet(ptrDarr, index, &item);
-    printf("%d\n",item);*/
+    item=5;
+    drraySet(ptrDarr, index,item);
+    printDarray(ptrDarr);
     darraySort(ptrDarr);
     printDarray(ptrDarr);
     
-
     return 0;
 }
